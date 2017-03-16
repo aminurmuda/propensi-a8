70b6cf927ab061aaceff2ba9a4f2f3afbddb8c6c
lutfimlna <lutfimlna11@gmail.com>
2017-03-16 17:00:11 +0700

    Update view, menampilkan pegawai, dan mencari pegawai

---
+++ resources/views/listpegawai.blade.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kelola Tim Akreditasi - Modul Akreditasi</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ url('css/akreditasi.css') }}" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <div class="navbar-wrapper">
+    <div class="container-fluid">
+        <nav class="navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img class="img-responsive" id="logonav" src="{{ url('images/logo.png') }}" alt="" >
+                    </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="">Home</a></li>
+                        
+                        <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Borang Standar <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Standar 2</a></li>
+                                <li><a href="#">Standar 4</a></li>
+                                <li><a href="#">Standar 7</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li><a href="#">Riwayat Akreditasi</a></li>
+                        <li class="active"><a href="#">Kelola Tim Akreditasi </a>
+                           
+                        </li>
+                       
+                    </ul>
+                    <ul class="nav navbar-nav pull-right">
+                        <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Signed in as  <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                
+                                <li><a href="#">My Profile</a></li>
+                            </ul>
+                        </li>
+                        <li class=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
+
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div  class="col-md-8 col-md-offset-2" id="kontenkelola">
+
+                <div id="titlekelola"> 
+                     <img class="img-responsive" src="{{ url('images/kelolatim.png') }}" alt="" >
+                     
+                     <br>
+                </div>
+
+                <div class="col-md-12 col-md-offset-3">
+                    <form action="{username}" method="POST" id="form-cari">
+                      {{ csrf_field() }}
+                      <div class="input-group">
+                         <input class="btn btn-md inputform" name="username" id="username" type="username" placeholder="Username" >
+                        
+                         <button class="btn btn-info btn-md" type="submit">Cari</button>
+                    
+                      </div>
+                    </form>
+
+
+                    <form action="#" method="POST"> 
+                      <div class="input-group">
+                         <input class="btn btn-md inputform" name="nip" id="nip" type="nip" placeholder="NIP" >
+
+                         <button class="btn btn-info btn-md" type="submit">Cari</button>
+                        
+                      </div>
+                    </form>
+                </div>
+
+
+                 <!-- <img class="img-responsive center-block" id="kotakbiru" src="images/kotakdaftarpengguna.png" alt="" > -->
+
+                 <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>NIP</th>
+                            <th>Edit</th>
+                    
+                        </tr>
+                    </thead>
+                  
+                    <tbody>
+                        @foreach($pegawai as $pegawai)
+                        <tr>
+                            <td>{{$pegawai->nama}}</td>
+                            <td>{{$pegawai->no_pegawai}}</td>
+                            <td>
+                                <center><button class="glyphicon glyphicon-trash" type="submit"></button></center>
+                            </td>
+                           
+                        </tr>
+                        @endforeach
+<!--                         <tr>
+                            <td>Ani nini</td>
+                            <td>123333</td>
+                            <td><button class="glyphicon glyphicon-trash" type="submit"></button>
+                            </td>
+                           
+                        </tr>
+                        <tr>
+                            <td>Ashton Cox</td>
+                            <td>156352</td>
+                            <td>
+                                <button class="glyphicon glyphicon-trash" type="submit"></button>
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>Cedric Kelly</td>
+                            <td>18249834</td>
+                            <td>
+                                <button class="glyphicon glyphicon-trash" type="submit"></button>
+                            </td>
+                        
+                        </tr>
+                        <tr>
+                            <td>Airi Satou</td>
+                            <td>122222</td>
+                            <td>
+                                <button class="glyphicon glyphicon-trash" type="submit"></button>
+                            </td>
+                           
+                        </tr>
+                        <tr>
+                            <td>Brielle Williamson</td>
+                            <td>1256362</td>
+                            <td>
+                                <button class="glyphicon glyphicon-trash" type="submit"></button>
+                            </td>
+                           
+                        </tr> -->
+
+                    </tbody>
+                </table> 
+
+                <br>
+
+                <div id="savetable" >
+                    <button class="btn btn-info btn-md col-md-offset-4" type="submit">Save</button>
+                    <button class="btn btn-info btn-md col-md-offset-1" type="submit">Cancel</button>
+                </div>
+                <br>
+                <br>
+
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+
+<footer>
+  <div class="container">
+    <p id="footerbawah">Copyright © 2017 Propensi A08. All Rights Reserved</p>
+  </div>
+</footer>
+
+
+
+    <!-- jQuery -->
+    <script src="{{ url('js/jquery.js') }}"></script>
+
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ url('js/bootstrap.min.js') }}">
+    </script>
+
+    <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"> </script>
+    <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"> </script>
+
+    <script>
+      $(document).ready(function() {
+        $('#example').DataTable();
+
+        // TODO===================
+        $('#username').on('change', function() {
+            $('#form-cari').attr('action', $('#username').val());
+        });
+    } );
+    </script>
+
+    <!-- script untuk navigation -->
+    <script> 
+            $(function(){
+            $('.button-checkbox').each(function(){
+                var $widget = $(this),
+                    $button = $widget.find('button'),
+                    $checkbox = $widget.find('input:checkbox'),
+                    color = $button.data('color'),
+                    settings = {
+                            on: {
+                                icon: 'glyphicon glyphicon-check'
+                            },
+                            off: {
+                                icon: 'glyphicon glyphicon-unchecked'
+                            }
+                    };
+
+                $button.on('click', function () {
+                    $checkbox.prop('checked', !$checkbox.is(':checked'));
+                    $checkbox.triggerHandler('change');
+                    updateDisplay();
+                });
+
+                $checkbox.on('change', function () {
+                    updateDisplay();
+                });
+
+                function updateDisplay() {
+                    var isChecked = $checkbox.is(':checked');
+                    // Set the button's state
+                    $button.data('state', (isChecked) ? "on" : "off");
+
+                    // Set the button's icon
+                    $button.find('.state-icon')
+                        .removeClass()
+                        .addClass('state-icon ' + settings[$button.data('state')].icon);
+
+                    // Update the button's color
+                    if (isChecked) {
+                        $button
+                            .removeClass('btn-default')
+                            .addClass('btn-' + color + ' active');
+                    } 
+                    else 
+                    { 
+                        $button
+                            .removeClass('btn-' + color + ' active')
+                            .addClass('btn-default');
+                    }
+                }
+                function init() {
+                    updateDisplay();
+                    // Inject the icon if applicable
+                    if ($button.find('.state-icon').length == 0) {
+                        $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+                    }
+                }
+                init();
+            });
+        });
+    </script>
+
+</body>
+
+</html>
