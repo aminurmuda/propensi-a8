85e4fdeee90fcd788dc45bf327aea682e6b861c2
aminurmuda <aminurmuda@gmail.com>
2017-03-16 20:15:04 +0700

    test middleware

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,13 @@ Route::get('login', 'MainController@login');
 // Route untuk logout
 Route::get('logout', 'MainController@logout');
 
-Route::get('timakreditasi/{kode_fakultas}', 'PegawaiController@lihatPengguna');
+//Route::get('timakreditasi/{kode_fakultas}', 'PegawaiController@lihatPengguna');
 
-Route::get('timakreditasi/{kode_prodi}', 'MainController@lihatPengguna');
-Route::get ('hapus/{kode_prodi}/{username}', 'MainController@hapusPengguna');
+//Route::get('timakreditasi/{kode_prodi}', 'MainController@lihatPengguna');
 
-Route::get('timakreditasi/{kode_prodi}', 'PegawaiController@lihatPengguna');
+//Route::get ('hapus/{kode_prodi}/{username}', 'MainController@hapusPengguna');
+
+//Route::get('timakreditasi/{kode_prodi}', 'PegawaiController@lihatPengguna');
 
 Route::get('timakreditasi/{kode_fakultas}', 'PegawaiController@lihatPengguna');
 
