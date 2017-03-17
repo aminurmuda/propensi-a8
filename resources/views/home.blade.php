78033c0639b9f4e9a85fb40a50eb764b56030fd1
Ristya Nintyana <rstya.ristya@gmail.com>
2017-03-17 16:16:18 +0700

    update homepage + css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,6 +85,85 @@
 
 
     <!-- Page Content -->
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-0" >
+            <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-one">
+                    <div class="price">
+                       <!-- <p class="plan-name"></p> -->
+                       <br>
+                        <p> STANDAR 2 </p>
+                        
+                    </div>
+                    <div class="plan-div">
+                        <p class="plan-text">
+                            <br>
+                            Lihat dan Ubah Borang Standar 2
+                        </p>
+                    </div>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-m round">Pilih Standar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-md-offset-0">
+            <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-one">
+                    <div class="price">
+                        <br>
+                        <p>STANDAR 4</p>
+                        
+
+                    </div>
+                    <div class="plan-div">
+                        <p class="plan-text">
+                            <br>Lihat dan Ubah Borang Standar 4
+                        </p>
+                    </div>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-m round">Pilih Standar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-md-offset-0">
+            <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-one">
+                    <div class="price">
+                        <br>
+                        <p>STANDAR 7</p>
+                        
+
+                    </div>
+
+                    <div class="plan-div">
+                        <p class="plan-text">
+                            <br>Lihat dan Ubah Borang Standar 7
+                        </p>
+                    </div>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-m round">Pilih Standar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   <br>
+   <br>
+   <br>
+   <br>
+
+
+
+</div>
+
     
     <!-- /.container -->
 
