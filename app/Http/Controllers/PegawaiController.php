772039bb7f9507903c2d91b22582896b866bcb9a
aminurmuda <aminurmuda@gmail.com>
2017-03-19 17:36:01 +0700

    fix tambah dan hapus tim akreditasi

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -20,9 +20,13 @@ class PegawaiController extends Controller
 	 */
 	public function tambahTimAkreditasi($username, Request $request) {
 		Pegawai::addTimAkreditasi($username);
+		$pegawai = Pegawai::getPegawaiByUsername($username);
+		$kodeFakultas = Pegawai::getFakultasPegawai($username);
 		return view('tambah-sukses', [
 			'role' => $request->session()->get('role'),
-            'user' => $request->session()->get('user'),        
+            'user' => $request->session()->get('user'),
+            'pegawai' => $pegawai,      
+            'kode_fakultas' => $kodeFakultas,  
             'username' => $username
 		]);
 	}
@@ -35,9 +39,13 @@ class PegawaiController extends Controller
 	 */
     public function deleteTimAkreditasi($username, Request $request){
     	Pegawai::deleteTimAkreditasi($username);
+    	$pegawai = Pegawai::getPegawaiByUsername($username);
+    	$kodeFakultas = Pegawai::getFakultasPegawai($username);
 		return view('hapus-sukses', [
 			'role' => $request->session()->get('role'),
             'user' => $request->session()->get('user'),
+            'kode_fakultas' => $kodeFakultas,
+            'pegawai' => $pegawai,
             'username' => $username
 		]);
     }
