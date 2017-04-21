90ceb4242ebd2448cfd028bd1f8da41cc2e66c42
ristyanintyana <rstya.ristya@gmail.com>
2017-04-21 13:40:52 +0700

    tambah view3a4

---
+++ resources/views/view3a4.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.headerfooter')
+
+@section('content')
+
+    <!-- Page Content -->
+
+    <div class="container">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <h3>Borang 3A, Standar 4</h3>
+            <span> Sumber Daya Manusia </span>
+            <button class="btn-primary btn-lg pull-right glyphicon glyphicon-pencil"> Edit</button>
+            <br><br><br>
+            <div class="panel-group wrap" id="bs-collapse">
+
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#one">
+         
+         4.1  Sistem Seleksi dan Pengembangan
+        </a>
+      </h4>
+                    </div>
+                    <div id="one" class="panel-collapse collapse">
+                        <div class="panel-body">
+                           
+
+                            <div class="form-group">
+                              <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
+
+                              </textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- end of panel -->
+
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#two">
+       4.2  Monitoring dan Evaluasi
+        </a>
+      </h4>
+                    </div>
+                    <div id="two" class="panel-collapse collapse">
+                        <div class="panel-body">
+
+                              Jelaskan sistem monitoring dan evaluasi, serta rekam jejak kinerja akademik dosen dan kinerja tenaga kependidikan (termasuk informasi tentang ketersediaan pedoman tertulis, dan monitoring dan evaluasi kinerja dosen dalam tridarma serta dokumentasinya).
+                              
+                             <div class="form-group">
+                              <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
+
+                              </textarea>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                <!-- end of panel -->
+
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#three">
+          2.3 Sistem Pengelolaan
+        </a>
+      </h4>
+                    </div>
+                    <div id="three" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            Sistem pengelolaan fungsional dan operasional program studi mencakup <i>planning, organizing, staffing, leading, controlling</i> dalam kegiatan  internal maupun eksternal.
+
+                            <br><br>
+
+                            Jelaskan sistem pengelolaan Program Studi serta dokumen pendukungnya.
+                            <div class="form-group">
+                              <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
+
+                              </textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of panel -->
+
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#four">
+         2.4 Penjaminan Mutu
+        </a>
+      </h4>
+                    </div>
+                    <div id="four" class="panel-collapse collapse ">
+                        <div class="panel-body">
+
+                            Bagaimanakah pelaksanaan penjaminan mutu pada Program Studi? Jelaskan.
+                           <div class="form-group">
+                              <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
+
+                              </textarea>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- end of panel -->
+
+
+                 <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#five">
+          2.5 Umpan Balik
+        </a>
+      </h4>
+                    </div>
+                    <div id="five" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            Apakah program studi telah melakukan kajian tentang proses pembelajaran melalui umpan balik dari dosen, mahasiswa, alumni, dan pengguna lulusan mengenai harapan dan persepsi mereka?  Jika Ya, jelaskan isi umpan balik dan tindak lanjutnya.
+                        </div>
+                    </div>
+                </div>
+                <!-- end of panel -->
+
+                 <div class="panel">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#bs-collapse" href="#six">
+          2.6 Keberlanjutan
+        </a>
+      </h4>
+                    </div>
+                    <div id="six" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            Jelaskan upaya untuk menjamin keberlanjutan <i>(sustainability)</i> program studi ini, khususnya dalam hal:<br>
+                            a. Upaya untuk peningkatan animo calon mahasiswa:<br>
+                            b. Upaya peningkatan mutu manajemen: <br>
+
+                            c. Upaya untuk peningkatan mutu lulusan:<br>
+
+                            d. Upaya untuk pelaksanaan dan hasil kerjasama kemitraan:<br>
+
+                            e. Upaya dan prestasi memperoleh dana hibah kompetitif: <br>
+
+                            <div class="form-group">
+                              <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
+
+                              </textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of panel -->
+
+            </div>
+            <!-- end of #bs-collapse  -->
+            <br><br><br>
+
+        </div>
+
+
+
+
+    </div>
+    <!-- end of container -->
+
+    <!-- /.container -->
+
+@endsection
