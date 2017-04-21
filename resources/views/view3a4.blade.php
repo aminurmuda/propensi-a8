b437c1f6113e51aa1eb98fd8cd6826eca869d8f9
gemmaan <gemmaan.adhatien@gmail.com>
2017-04-21 20:08:27 +0700

    update standar 4 3a

--- resources/views/view3a4.blade.php
+++ resources/views/view3a4.blade.php
@@ -24,7 +24,6 @@
                     <div id="one" class="panel-collapse collapse">
                         <div class="panel-body">
                            
-
                             <div class="form-group">
                               <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
 
@@ -66,17 +65,46 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#bs-collapse" href="#three">
-          2.3 Sistem Pengelolaan
+          4.3 Dosen Tetap
         </a>
       </h4>
                     </div>
                     <div id="three" class="panel-collapse collapse">
                         <div class="panel-body">
-                            Sistem pengelolaan fungsional dan operasional program studi mencakup <i>planning, organizing, staffing, leading, controlling</i> dalam kegiatan  internal maupun eksternal.
-
+                            Dosen tetap dalam borang akreditasi BAN-PT adalah dosen yang diangkat dan ditempatkan sebagai tenaga tetap pada Perguruan Tinggi yang bersangkutan; termasuk dosen penugasan Kopertis, dan dosen yayasan pada Perguruan Tinggi Swasta dalam bidang yang relevan dengan keahlian bidang studinya. Seorang dosen hanya dapat menjadi dosen tetap pada satu perguruan tinggi, dan mempunyai penugasan kerja minimum 20 jam/minggu.
                             <br><br>
 
-                            Jelaskan sistem pengelolaan Program Studi serta dokumen pendukungnya.
+                            Dosen tetap dipilah dalam 2 kelompok, yaitu:
+
+                            1) Dosen tetap yang bidang keahliannya sesuai dengan Program Studimanajemen.
+                            2) Dosen tetap yang bidang keahliannya di luar Program Studi manajemen
+
+                            <br>
+                            4.3.1Data dosen tetap yang bidang keahliannya sesuai dengan bidang Program Studi:
+                            <br>
+                            4.3.2 Data dosen tetap yang bidang keahliannya di luar bidang Program Studi :
+                            <br>
+                            4.3.3 Aktivitas dosen tetap yang bidang bidang keahliannya sesuai dengan Program Studi dinyatakan dalam sks rata-rata per semester pada satu tahun akademik terakhir, diisi dengan perhitungan sesuai SK Dirjen DIKTI no. 48 tahun 1983 (12 sks setara dengan 36 jam kerja per minggu)
+
+                            <br>
+                            Catatan:
+                            <br>
+                            SKS pengajaran sama dengan sks mata kuliah yang diajarkan. Bila dosen mengajar kelas paralel, maka beban sks pengajaran untuk satu tambahan kelas paralel adalah 1/2 kali sks mata kuliah. <br>
+                            * rata-rata adalah jumlah sks dibagi dengan jumlah dosen tetap. <br>
+                            ** sks manajemen dihitung sbb : <br>
+                            Beban kerja manajemen untuk jabatan-jabatan ini adalah sbb.<br>
+                           - rektor/direktur politeknik 12 sks<br>
+                            - pembantu rektor/dekan/ketua sekolah tinggi/direktur akademi 10 sks<br>
+                            - ketua lembaga/kepala UPT 8 sks <br>
+                            - pembantu dekan/ketua jurusan/kepala pusat/ketua senat akademik/ketua senat fakultas 6 sks <br>
+                            - sekretaris jurusan/sekretaris pusat/sekretaris senat akademik/sekretaris senat universitas/sekretaris senat<br>
+                            - fakultas/ kepala lab. atau studio/kepala balai/ketua Program Studi 4 sks<br>
+                            - sekretaris Program StudiS 3 sks<br><br>
+                            Bagi Perguruan Tinggi yang memiliki struktur organisasi yang berbeda, beban kerja manajemen untuk jabatan baru disamakan dengan beban kerja jabatan yang setara.                            
+                            <br>
+                            <br>
+                            4.3.4 Data aktivitas mengajar dosen tetap yang bidang keahliannya sesuai dengan Program Studi, dalam satu tahun akademik terakhir di Program Studi ini dengan mengikuti format tabel berikut :<br>
+                            4.3.5 Data aktivitas mengajar dosen tetap yang bidang keahliannya di luar Program Studi, dalam satu tahun akademik terakhir di Program Studi ini dengan mengikuti format tabel berikut: <br>
                             <div class="form-group">
                               <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
 
@@ -91,14 +119,15 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#bs-collapse" href="#four">
-         2.4 Penjaminan Mutu
+         4.4 Dosen Tidak Tetap
         </a>
       </h4>
                     </div>
                     <div id="four" class="panel-collapse collapse ">
                         <div class="panel-body">
 
-                            Bagaimanakah pelaksanaan penjaminan mutu pada Program Studi? Jelaskan.
+                            4.4.1 Tuliskan data dosen tidak tetap pada Program Studi dengan mengikuti format tabel berikut: <br>
+                            4.4.2Data aktivitas mengajar dosen tidak tetap pada satu tahun terakhir di Program Studi ini dengan mengikuti format tabel berikut:
                            <div class="form-group">
                               <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
 
@@ -115,13 +144,49 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#bs-collapse" href="#five">
-          2.5 Umpan Balik
+          4.5 Upaya Peningkatan Sumber Daya Manusia (SDM) dalam tiga tahun terakhir
         </a>
       </h4>
                     </div>
                     <div id="five" class="panel-collapse collapse">
                         <div class="panel-body">
-                            Apakah program studi telah melakukan kajian tentang proses pembelajaran melalui umpan balik dari dosen, mahasiswa, alumni, dan pengguna lulusan mengenai harapan dan persepsi mereka?  Jika Ya, jelaskan isi umpan balik dan tindak lanjutnya.
+                    4.5.1 Kegiatan tenaga ahli/pakar sebagai pembicara dalam seminar/pelatihan, pembicara tamu, dsb, dari luar Perguruan Tinggi sendiri (tidak termasuk dosen tidak tetap)<br><br>
+                    <div class="table-responsive">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Nama Tenaga Ahli/Pakar</th>
+                          <th>Nama dan Judul Kegiatan</th>
+                          <th>Waktu Pelaksanaan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>Larry</td>
+                          <td>the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>                    
+                    </div>
+                    4.5.2. Peningkatan kemampuan dosen tetap melalui program tugas belajar dalam bidang yang sesuai dengan bidang Program Studi<br>
+                    4.5.3. Kegiatan dosen tetap yang bidang keahliannya sesuai dengan Program Studi dalam seminar ilmiah/ lokakarya/ penataran/ workshop/ pagelaran/ pameran/ peragaan yang tidak hanya melibatkan dosen Perguruan Tinggi sendiri.<br>
+                    4.5.4 Pencapaian prestasi/reputasi dosen (misalnya prestasi dalam pendidikan, penelitian dan pelayanan/pengabdian kepada masyarakat).<br>
+                    4.5.5 Keikutsertaan dosen tetap dalam organisasi keilmuan atau organisasi profesi.<br>
                         </div>
                     </div>
                 </div>
@@ -131,21 +196,15 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#bs-collapse" href="#six">
-          2.6 Keberlanjutan
+          4.6 Tenaga Kependidikan
         </a>
       </h4>
                     </div>
                     <div id="six" class="panel-collapse collapse">
                         <div class="panel-body">
-                            Jelaskan upaya untuk menjamin keberlanjutan <i>(sustainability)</i> program studi ini, khususnya dalam hal:<br>
-                            a. Upaya untuk peningkatan animo calon mahasiswa:<br>
-                            b. Upaya peningkatan mutu manajemen: <br>
-
-                            c. Upaya untuk peningkatan mutu lulusan:<br>
-
-                            d. Upaya untuk pelaksanaan dan hasil kerjasama kemitraan:<br>
-
-                            e. Upaya dan prestasi memperoleh dana hibah kompetitif: <br>
+                    4.6.1 Tenaga kependidikan
+                    <br>Tuliskan data tenaga kependidikan yang ada di Program Studi, Jurusan, Fakultas atau Perguruan Tinggi yang melayani mahasiswa Program Studi dengan mengikuti format tabel berikut:<br>
+                    4.6.2 Jelaskan upaya yang telah dilakukan Program Studi dalam meningkatkan kualifikasi dan kompetensi tenaga kependidikan. <br>
 
                             <div class="form-group">
                               <textarea class="form-control" rows="5" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis risus condimentum eros dapibus, elementum molestie purus bibendum. Vivamus malesuada vel urna vel dapibus. Nam et ligula varius, scelerisque urna eget, blandit quam. Aliquam ex elit, sollicitudin et laoreet sit amet, tristique ac odio. Aenean dignissim justo augue, sit amet consequat mi scelerisque ac. Nam lobortis tristique est eget aliquam. Sed sit amet vestibulum lectus, in congue magna.
