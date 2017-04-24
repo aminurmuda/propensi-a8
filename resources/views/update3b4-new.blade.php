fd1e71c64a3f34c92552f2a0fe7b58289b5bba0b
gemmaan <gemmaan.adhatien@gmail.com>
2017-04-24 19:45:10 +0700

    3b4 kualitatif

--- resources/views/update3b4-new.blade.php
+++ resources/views/update3b4-new.blade.php
@@ -8,21 +8,22 @@
     <div class="row">
         <div class="col-md-12" >
             <div class="db-wrapper">
-            <h1>Update Borang 3A Standar {{$kodeStandarStr}}</h1><br>
-            @if($kodeStandarStr!='4.6.2')
+            <h1>Update Borang 3B Standar {{$kodeStandarStr}}</h1><br>
+            <!-- 4.1.3,4.2 -->
+            @if($kodeStandarStr!='4.1.3')
             <p>{{$standar4['standar4'][$kodeStandarStr]['perintah'] }}</p>
             @else
-            <p>{{$standar4['standar4']['4.6'][$kodeStandarStr]['perintah'] }}</p>
+            <p>{{$standar4['standar4']['4.1'][$kodeStandarStr]['perintah'] }}</p>
             @endif
 
             <br>
-                          <form action="{{url('3a/standar4edit/'.$kodeStandar.'/'.$kodeProdi.'/3a/submit')}}">
+                          <form action="{{url('3b/standar4edit/'.$kodeStandar.'/'.$kodeFakultas.'/3b/submit')}}">
                           {{csrf_field()}}
                           <div class="form-group">
-                            <textarea class="form-control" id='textarea' name='textarea'>@if($kodeStandarStr!='4.6.2')
+                            <textarea class="form-control" id='textarea' name='textarea'>@if($kodeStandarStr!='4.1.3')
                             <p>{{$standar4['standar4'][$kodeStandarStr]['isian'] }}</p>
                             @else
-                            <p>{{$standar4['standar4']['4.6'][$kodeStandarStr]['isian'] }}</p>
+                            <p>{{$standar4['standar4']['4.1'][$kodeStandarStr]['isian'] }}</p>
                             @endif
                             </textarea>
                           </div>
