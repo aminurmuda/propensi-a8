cc30acb47f9414db72397f5f9c8620b24e32cb1c
gemmaan <gemmaan.adhatien@gmail.com>
2017-05-17 17:28:36 +0700

    benerin error homestatus

--- app/Borang.php
+++ app/Borang.php
@@ -84,7 +84,7 @@ class borang extends Model
             ->join('program_studi', 'histori_akreditasi.kode_prodi', '=', 'program_studi.kode_prodi')
             ->select('borang.id_histori','borang.isi','borang.jenis','borang.standar', 'borang.tahun', 'program_studi.nama_prodi','program_studi.kode_prodi','borang.kode_prodi as kodeProdiFakultas','borang.is_reviewed as status')
             ->where('borang.kode_prodi',$kode_prodi)
-            ->groupBy('borang.id_histori','borang.isi','borang.jenis','borang.standar', 'borang.tahun', 'program_studi.nama_prodi','program_studi.kode_prodi','borang.is_reviewed')
+            ->groupBy('borang.id_histori','borang.isi','borang.jenis','borang.standar', 'borang.tahun', 'program_studi.nama_prodi','program_studi.kode_prodi','borang.kode_prodi','borang.is_reviewed')
             ->get();
     }
 
